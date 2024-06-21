9c4e509620df40b39bff27e92e869520ff11f116
Jack Sabine <jack@sabine.ws>
2024-06-20 21:44:34 -0400

    wip

--- src/Rummikub_Tile_Set.cpp
+++ src/Rummikub_Tile_Set.cpp
@@ -7,12 +7,12 @@ Rummikub_Tile_Set::Rummikub_Tile_Set(
     uint8_t duplicates,
     bool has_wilds
 ) : suit_count(suit_count), tiles_per_suit(tiles_per_suit) {
-    assert(this->tiles_per_suit < 64);
+    assert(this->tiles_per_suit < 63); // must be a gap between groups of 64
 
     for (uint8_t d = 0; d < duplicates; d++) {
         for (uint8_t s = 0; s < suit_count; s++) {
             for (uint8_t t = 0; t < tiles_per_suit; t++) {
-                    tiles.push_back(suit_and_num_to_tile_index(s, t));
+                tiles.push_back(suit_and_num_to_tile_index(s, t));
             }
         }
 
@@ -28,6 +28,8 @@ tile_t Rummikub_Tile_Set::suit_and_num_to_tile_index(uint8_t suit, uint8_t num) 
 
 std::string Rummikub_Tile_Set::tile_index_to_suit_and_num(tile_t index) {
     uint8_t suit, num;
+
+    if (index == WILD_INDEX) return "WILD";
 
     suit = index >> 6;
     num = index & (0b111111u);
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include <stdlib.h>
 #include <iostream>
+#include <algorithm>
 #include "Rummikub_Tile_Set.h"
 
 int main(void) {
     Rummikub_Tile_Set tile_set(4, 13, 2, true);
     std::vector<tile_t> hand;
 
-    tile_set.shuffle_tiles();
-    hand = tile_set.pick_first_n_tiles(14);
+    std::cout << "----------\n";
 
-    for (tile_t tile : hand) {
-        std::cout << tile_set.tile_index_to_suit_and_num(tile) << "\n";
+    for (uint8_t i = 0; i < 8; i++) {
+        tile_set.shuffle_tiles();
+        hand = tile_set.pick_first_n_tiles(14);
+
+        std::sort(hand.begin(), hand.end());
+
+        for (tile_t tile : hand) {
+            std::cout << tile_set.tile_index_to_suit_and_num(tile) << "\n";
+        }
+
+        std::cout << "----------\n";
     }
 
     return EXIT_SUCCESS;
